196516d3b780717f6bfb5aaaf86e652fe9e4b275
Travis Tincher <travistincher@Traviss-MacBook-Air.local>
2020-09-14 15:35:19 -0500

    corrected typo in banner

--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -1,15 +1,16 @@
-import React from 'react';
-import './Banner.css';
+import React from "react";
+import "./Banner.css";
 
 function Banner() {
-	return (
-		<div id="banner">
-			<h1>
-				A software user experience should be like trying a new craft beer. You are intrigued by the label,
-				convince by the first sip, and left wanting more.
-			</h1>
-		</div>
-	);
+  return (
+    <div id="banner">
+      <h1>
+        A software user experience should be like trying a new craft beer. You
+        are intrigued by the label, convinced by the first sip, and left wanting
+        more.
+      </h1>
+    </div>
+  );
 }
 
 export default Banner;
